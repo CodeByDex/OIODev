e5412feb70798cca81412d3c817371133fca5a48
that devguy <zkmutch@gmail.com>
2023-06-28 15:40:52 -0400

    small styling changes to fit better when doing mobile first design -- including a globalcss for margins on main elements

--- src/app/[user]/page.js
+++ src/app/[user]/page.js
@@ -3,7 +3,7 @@
 export default function Services() {
   return (
     <main className="flex">
-      <h1 className="font-primary text-brand-textHeader text-6xl">
+      <h1 className="font-primary text-brand-textHeader text-2xl m-w-full">
         User Dashboard Page
       </h1>
     </main>
