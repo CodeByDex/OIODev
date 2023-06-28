102bf29c9069581e62ebc86676eeb87cd6552d4b
that devguy <zkmutch@gmail.com>
2023-06-28 18:35:14 -0400

    started working on the DevCards component

--- src/app/services/components/DevCards.js
+++ src/app/services/components/DevCards.js
@@ -1,7 +1,55 @@
+"use client";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRightToBracket,
+  faUser,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function DevCards() {
-    return (
+  return (
+    <section className="devCards--container flex gap-4 bg-slate-200/5 rounded-lg p-5">
+      <div className="devCards--imageContainer">
+        <div className="devCards--imagePlaceholder h-20 w-20 bg-brand-accent rounded-full" />
+      </div>
+      <div className="devCards--infoContainer flex-col w-fit items-center">
+        {/* Info */}
         <div>
-            Enter
+          <h2 className="devCards--name text-xl text-brand-textHeader">
+            Dave Developer
+          </h2>
+          <h4 className="devCards--title text-xs">Full-stack Developer</h4>
         </div>
-    );
+        {/* Rates */}
+        <div className="devCards--rates leading-4 my-2">
+          <h3 className="text-brand-textHeader">$300 - $1000</h3>
+          <p>Rates</p>
+        </div>
+        {/* Bio */}
+        <p className="devCards--bio text-sm">
+          <span className="text-brand-secondary font-bold">BIO</span>
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        {/* Social Links */}
+        <div className="devCards--socialsContainer flex items-center gap-2">
+          <FontAwesomeIcon
+            icon={faLinkedinIn}
+            className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
+          />
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
+          />
+        </div>
+        <p></p>
+      </div>
+    </section>
+  );
 }
--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,11 +1,13 @@
 //This page returns a listing of all portfolios and displays the summary 'card'
+import DevCards from "./components/DevCards";
 
 export default function Services() {
   return (
-    <main className="flex">
+    <main className="flex-col">
       <h1 className="font-primary text-brand-textHeader text-2xl m-w-full">
         Services Page
       </h1>
+      <DevCards />
     </main>
   );
 }
