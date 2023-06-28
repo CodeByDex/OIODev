ef208eceb11b694e26881513f4ce3979cfcaf12f
that devguy <zkmutch@gmail.com>
2023-06-28 13:05:29 -0400

    stubbed pages with blank standard styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="font-primary text-brand-textHeader text-6xl">OIO Dev</h1>
-      <p className="font-secondary text-3xl">
+    <main className="flex">
+      <h1 className="font-primary text-brand-textHeader text-6xl">
         Delivering high-quality, user-friendly and mobile responsive web
         applications that embody the essence of your brand.
-      </p>
+      </h1>
     </main>
   );
 }
