e421bfdaa5e32e237c11455d99ffa41caf4cdcf0
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-06-29 19:02:11 -0400

    Added check if model is already registered, if it is, then don't re-regrister it

--- src/models/User.js
+++ src/models/User.js
@@ -16,6 +16,6 @@ const userSchema = new Schema(
     }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 module.exports = User;
