34886447cbb951f5a6c8d0449bbf6df43f348c8d
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-06-29 12:23:18 -0400

    Set up basic structure for Login & SignUp + imported them into index.js file

--- src/components/login/SignupForm.js
+++ src/components/login/SignupForm.js
@@ -1,1 +1,16 @@
 //TODO - add form for signup
+import React, { useState } from 'react';
+
+function SignUp() {
+
+
+    return (
+        <main>
+            <div>
+                <h1>SignUp Form</h1>
+            </div>
+        </main>
+    )
+};
+
+export default SignUp;
