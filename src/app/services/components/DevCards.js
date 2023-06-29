8288b7be34605deae50e19ba44c08ff0f2bda1a5
that devguy <zkmutch@gmail.com>
2023-06-29 15:10:51 -0400

    updated temp data with names, added responsiveness to the DevCard component

--- src/app/services/components/DevCards.js
+++ src/app/services/components/DevCards.js
@@ -6,10 +6,10 @@ import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function DevCards(props) {
   return (
-    <section className="devCards--container flex bg-gray-200/5 rounded-lg mb-6 p-5">
-      <div className="devCards--infoContainer flex-col items-center">
+    <section className="devCards--container flex bg-gray-200/5 rounded-lg mb-6 mx-auto p-5 md:px-8 md:max-w-3xl">
+      <div className="devCards--infoContainer flex flex-col items-center md:flex-row md:gap-4">
         {/* Info */}
-        <div className="flex gap-4">
+        <div className="flex mr-auto gap-4 md:w-2/5">
           <div className="devCards--imageContainer flex items-center">
             <div className="devCards--imagePlaceholder h-24 w-24 bg-brand-accent rounded-full" />
           </div>
@@ -24,7 +24,7 @@ export default function DevCards(props) {
                 <FontAwesomeIcon icon={faCircle} className="p-2 w-2 h-2 z-10" />
               </div>
             </p>
-            <h2 className="devCards--name text-2xl leading-6 text-brand-textHeader">
+            <h2 className="devCards--name text-2xl leading-6 text-brand-textHeader md:text-3xl">
               {props.name}
             </h2>
             <h4 className="devCards--title text-sm">{props.title}</h4>
@@ -37,33 +37,35 @@ export default function DevCards(props) {
           </div>
         </div>
         {/* Bio */}
-        <div className="devCards--bio text-sm my-2">
-          <div className="text-brand-secondary font-bold flex justify-between mb-1">
-            <p>BIO</p>
-            <Link
-              href={props.portfolioUrl}
-              target="_blank"
-              className="underline"
-            >
-              View My Portfolio
+        <div className="devCards--bioContainer md:w-3/5 md:mt-2">
+          <div className="text-sm my-2">
+            <div className="text-brand-secondary font-bold flex justify-between mb-1">
+              <p>BIO</p>
+              <Link
+                href={props.portfolioUrl}
+                target="_blank"
+                className="underline"
+              >
+                View My Portfolio
+              </Link>
+            </div>
+            <p>{props.bio}</p>
+          </div>
+          {/* Social Links */}
+          <div className="devCards--socialsContainer flex justify-end items-center gap-2">
+            <Link href={props.linkedinUrl} target="_blank">
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
+              />
+            </Link>
+            <Link href={props.githubUrl} target="_blank">
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
+              />
             </Link>
           </div>
-          <p>{props.bio}</p>
-        </div>
-        {/* Social Links */}
-        <div className="devCards--socialsContainer flex justify-end items-center gap-2">
-          <Link href={props.linkedinUrl} target="_blank">
-            <FontAwesomeIcon
-              icon={faLinkedinIn}
-              className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
-            />
-          </Link>
-          <Link href={props.githubUrl} target="_blank">
-            <FontAwesomeIcon
-              icon={faGithub}
-              className="bg-brand-secondary/5 text-brand-secondary p-2 rounded-lg w-5 h-5"
-            />
-          </Link>
         </div>
       </div>
     </section>
