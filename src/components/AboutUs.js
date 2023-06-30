18bd6e4114eb50ea55a7b3a93dc68a04985d203b
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-06-29 21:23:10 -0400

    added image to Mission component and worked on some styling

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,6 @@
 "use client"
 import Link from "next/link";
 import "tailwindcss/tailwind.css";
-import heroImage from "../../public/WebDevelopmentHero.jpg"
 
 
 export default function AboutUs() {
@@ -9,15 +8,13 @@ export default function AboutUs() {
     return (
         <main className="footer--container px-10 py-9 ">
 
-        <Link href="/heroImage">
-
-        </Link>
-        <span>
-        Welcome to OIODev, where we connect you with skilled IT professionals who specialize in creating exceptional websites and server applications.
-        By entrusting your project to a professional, you benefit from their expertise and experience, ensuring a polished and high-performing website. 
-        A modern and up-to-date website offers numerous advantages, including improved user experience, enhanced brand credibility, increased online visibility, and higher user engagement. 
-        Let us help you unlock the full potential of your online presence with a professional touch that captivates and engages your audience.
-        </span>
+            <span>
+            Welcome to OIODev, where we connect you with skilled IT professionals who specialize in creating exceptional websites and server applications.
+            By entrusting your project to a professional, you benefit from their expertise and experience, ensuring a polished and high-performing website. 
+            A modern and up-to-date website offers numerous advantages, including improved user experience, enhanced brand credibility, increased online visibility, and higher user engagement. 
+            Let us help you unlock the full potential of your online presence with a professional touch that captivates and engages your audience.
+            </span>
+            
         </main>
     )
 }
--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,21 +1,30 @@
 "use client"
 import Link from "next/link";
+import Image from "next/image";
 import "tailwindcss/tailwind.css";
-import heroImage from "../../public/WebDevelopmentHero.jpg"
+import heroImage from "../../public/WebDevHero.jpg"
 
 
 export default function Mission() {
 
     return (
-        <main className="footer--container px-10 py-9 ">
 
-        <Link href="/heroImage">
+        <main className="hero--mission--container sm:flex sm:flex-col md:flex md:flex-row px-10 py-9">
+        
+            <Image 
+                className="hero--image"
+                src={heroImage}
+                width={400}
+                height={300}
+                alt="Mission Hero Image"
+            />
+       
+            <span>
+            Delivering high-quality, user-friendly and mobile responsive web
+            applications that embody the essence of your brand.
+            </span>
 
-        </Link>
-        <span>
-        Delivering high-quality, user-friendly and mobile responsive web
-        applications that embody the essence of your brand.
-        </span>
         </main>
+
     )
 }
