3e241db7dd8e4ba572d59f1795b2d478ddcc1af7
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-07-01 18:44:32 -0400

    initial setup for next-auth for mongodb

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
     {
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../config/mongodb";
 
-export default NextAuth({
+export const authOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         })
-    ]
-})
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+    secret: process.env.NEXTAUTH_SECRET
+}
 
+export default NextAuth(authOptions)
