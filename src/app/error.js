e833b7fd2627eb0e317e56e3de9e01bf1ae35fdc
Zach Mutch <zkmutch@gmail.com>
2023-07-01 22:07:07 -0400

    added static svg for 404 page, starting building error.js - need to solve 404 routing, currently wildcard routes to [user]

--- src/app/error.js
+++ src/app/error.js
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Error({ error, reset }) {
   useEffect(() => {
@@ -9,6 +10,18 @@ export default function Error({ error, reset }) {
   return (
     <div>
       <h2>Something went wrong!</h2>
+      <div className="">
+        <h2>4</h2>
+        <Image
+          src="/static/space.svg"
+          alt="Space Illustration"
+          width={300}
+          height={300}
+          className=""
+        />
+        <h2>4</h2>
+      </div>
+
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
