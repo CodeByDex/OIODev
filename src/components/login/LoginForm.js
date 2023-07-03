2f084bbea6f3745fa214e02b83356d18b822930d
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-02 20:28:48 -0400

    setup basic export function

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,1 +1,25 @@
 //TODO - add form for login
+
+export default function loginForm() {
+
+
+
+    return (
+        <main>
+            <div>
+                <form>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                        placeholder="Email"
+                        name="Email"
+                        type="email"
+                        id="email"
+                        required 
+                        />
+                    </div>
+                </form>
+            </div>
+        </main>
+    )
+}
