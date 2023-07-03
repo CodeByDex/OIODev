26279b7cab655e48b8a8f7dda36041640a2b8cf7
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-02 20:49:13 -0400

    centered form in the center with some basic style change

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -5,21 +5,32 @@ export default function loginForm() {
 
 
     return (
-        <main>
-            <div>
+        <main className="flex flex-col gap-5">
+            <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center p-'>
                 <form>
-                    <div>
+                    <div className="font-secondary text-brand-secondary text-4xl">
                         <label htmlFor="email">Email:</label>
                         <input 
                         placeholder="Email"
-                        name="Email"
+                        name="email"
                         type="email"
                         id="email"
                         required 
+                        />
+                    </div>
+  
+                    <div className="font-secondary text-brand-secondary text-4xl">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                        placeholder="********"
+                        name="password"
+                        type="password"
+                        id="password"
+                        required
                         />
                     </div>
                 </form>
             </div>
         </main>
     )
-}
+  }
