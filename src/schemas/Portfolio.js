3a22924649bf41688817149c7e1a194a85da1312
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-07-02 21:16:41 -0400

    created mutation for updating a portfolio

--- src/schemas/Portfolio.js
+++ src/schemas/Portfolio.js
@@ -3,7 +3,21 @@ import { gql } from "@apollo/server";
 
 export const typeDef = gql`
     type Portfolio {
-        _id: ID
+        _id: ID!
+        firstName: String!
+        lastName: String!
+        title: String!
+        bio: String!
+        rateLow: Number!
+        rateHigh: Number!
+        portfolioUrl: String!
+        githubUrl: String
+        linkedinUrl: String
+        available: Boolean!
+    }
+
+    input portfolioInput {
+        _id: ID!
         firstName: String
         lastName: String
         title: String
@@ -13,15 +27,29 @@ export const typeDef = gql`
         portfolioUrl: String
         githubUrl: String
         linkedinUrl: String
-        available: Boolean
+        available: Boolean   
     }
 
     extend type Query {
         portfolio(ID: ID!): Portfolio
         portfolios: [Portfolio]
     }
+    
+    extend type Mutation($portfolio: portfolioInput) {
+        updatePortfolio(portfolio: $portfolio): Portfolio
+    }
 `
 
-export const resolvers = gql`
+export const resolvers = {
+    portfolios: async () => {
+        return await Portfolio.find({});
+    },
+    
+    portfolio: async (parent, { portfolioID }) => {
+        return await Portfolio.findById(portfolioID);
+    },
 
-`
+    updatePortfolio: async(parent, args) => {
+        return await Portfolio.findOneAndUpdate({_id: args.id }, {$set: {...args}});
+    }
+}
