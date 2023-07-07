18baae7480c49209413cbee4be61ab037449f585
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-07-06 20:28:28 -0400

    added userid field

--- src/schemas/Portfolio.js
+++ src/schemas/Portfolio.js
@@ -3,6 +3,7 @@ import { Portfolio } from "../models/";
 export const typeDef = `
     type Portfolio {
         _id: ID
+        userID: ID
         firstName: String
         lastName: String
         title: String
@@ -15,6 +16,7 @@ export const typeDef = `
     }
 
     input portfolioInput {
+        userID: ID
         firstName: String
         lastName: String
         title: String
