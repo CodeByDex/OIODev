9bdc69e766e2345e781ac3e173be49bf9d9bdc63
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-07-09 10:46:53 -0400

    Ryan helped me start over with the Calendly. We created a separate folder and files to contain the CalendlyForm and page.js

---
+++ src/app/calendly/page.js
@@ -0,0 +1,13 @@
+
+import CalendlyForm from "./components/CalendlyForm";
+
+export default function Calendly(){
+   //console.log(InlineWidget)
+   
+    return(
+        <main>
+            <p>Calendly</p>
+            <CalendlyForm />
+        </main>
+    )
+}
