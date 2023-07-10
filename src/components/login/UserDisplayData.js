93e4c0f441efc300834c3aebcf4daa383e94875d
Nathan Moon <moonman700@gmail.com>
2023-07-10 13:59:49 -0400

    added submenu toggling on profile image hover/click

--- src/components/login/UserDisplayData.js
+++ src/components/login/UserDisplayData.js
@@ -1,26 +1,63 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Image from "next/image"
+import { set } from "mongoose";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function UserDisplayData() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
   const Console = () => {
-    console.log(session)
+    console.log(session);
+  };
+  const [subNav, setSubNav] = useState(true);
+
+  const OpenSubNav = () => {
+    setSubNav(true)
   }
+
+  const CloseSubNav = () => {
+    setSubNav(false);
+  };
+
+  const toggleSubNav = () => {
+    setSubNav(!subNav);
+  };
+
   if (session) {
     return (
       <>
-        <p>Hello, {session.user.name}</p>
         <button onClick={() => Console()}>console.log</button>
-        <Image className=" rounded-full hover: bg-white" src={session.user.image}
-        width={30}
-        height={30} />
+        <Image
+          className=" rounded-full hover: bg-white mb"
+          onMouseEnter={OpenSubNav}
+          onClick={OpenSubNav}
+          src={session.user.image}
+          width={30}
+          height={30}
+        />
+
+        {/* submenu */}
+        <div
+          onClick={toggleSubNav}
+          onMouseEnter={OpenSubNav}
+          onMouseLeave={CloseSubNav}
+          className={
+            subNav
+              ? "fixed top-20 right-16 bg-brand-tertiary rounded-bl-2xl items-center p-5 duration-500"
+              : "fixed right-[-10%] top-24 p-5 duration-500 ease-in"
+          }
+        >
+          <p>Hello, {session.user.name}</p>
+          <p>dashboard</p>          
+          <button onClick={() => signOut()}>Sign out</button>
+          
+        </div>
       </>
-    )
+    );
   }
   return (
     <>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
-  )
+  );
 }
