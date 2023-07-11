4dc8c1a6cf8ca90b9e22df7c13dbc82b70585792
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-07-11 18:54:09 -0400

    fixed typo

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,23 +3,20 @@
 import { useEffect, useState } from 'react'; 
 import { getProviders, signIn } from 'next-auth/react';
 
-export default function loginPage() {
+export default function LoginPage() {
     const [data, setData] = useState([]);
 
-    function fetchProviders() {
-        useEffect(() => {
+    useEffect(() => {
 
-            const fetchData = async () => {
-                const data = await getProviders();
-                const providers = Object.values(data);
-                setData(providers);
-            }
+        const fetchData = async () => {
+            const data = await getProviders();
+            const providers = Object.values(data);
+            setData(providers);
+        }
 
-            fetchData();
-        }, [])
-    }
+        fetchData();
+    }, [])
 
-    fetchProviders();
     return (
         <main className="flex">
         <h1 className="font-primary text-brand-textHeader text-6xl">
