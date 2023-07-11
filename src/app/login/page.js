c12b0fb6285a3ed952c3d315409a249e7478a2e0
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-11 06:43:04 -0400

    seperated the providers into two buttons with basic styling

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -21,16 +21,35 @@ export default function loginPage() {
 
     fetchProviders();
     return (
-        <main className="flex">
-        <h1 className="font-primary text-brand-textHeader text-6xl">
-          {data.map(provider => (
-            <div key={provider.name}>
-                <button onClick={() => signIn(provider.id)}>
-                    Sign in with {provider.name}
-                </button>
-            </div>
-          ))}
-        </h1>
+
+        <main className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center p-'>
+        <div className='bg-brand-primary/60 px-6 py-8 rounded shadow-md text-black w-full'>
+          <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center p-'>
+              <form>
+                  <div className="font-secondary text-brand-secondary text-2xl">
+                     
+
+                  {data.map(provider => (
+                      <div key={provider.name}>
+                    <button className='w-full text-center py-3 rounded bg-brand-secondary text-white hover:bg-green-dark focus:outline-none my-1 text-xl' type="submit" onClick={() => signIn(provider.id)}> Sign in with: {provider.name}</button>
+                    </div>
+                  ))}
+                  </div>
+              </form>
+          </div>
+          </div>
+        
       </main>
+    //     <main className="flex">
+    //     <h1 className="font-primary text-brand-textHeader text-6xl">
+    //       {data.map(provider => (
+    //         <div key={provider.name}>
+    //             <button onClick={() => signIn(provider.id)}>
+    //                 Sign in with {provider.name}
+    //             </button>
+    //         </div>
+    //       ))}
+    //     </h1>
+    //   </main>
     )
 }
