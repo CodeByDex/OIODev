11d02c71b5258aaaaee69a9a73b181e2d08ecdb7
Christian Smith <christiansmith@Christians-MBP.lan>
2023-07-11 07:10:07 -0400

    styled the sign in

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -22,34 +22,21 @@ export default function loginPage() {
     fetchProviders();
     return (
 
-        <main className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center p-'>
+        <main className='container max-w-sm mx-auto my-auto flex-1 flex flex-col items-center justify-center'>
         <div className='bg-brand-primary/60 px-6 py-8 rounded shadow-md text-black w-full'>
-          <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center p-'>
-              <form>
+          <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-end'>
                   <div className="font-secondary text-brand-secondary text-2xl">
                      
-
+                  <h1 className='mb-8 mt-4 text-4xl text-center font-bold text-brand-secondary'>Sign In</h1>
                   {data.map(provider => (
                       <div key={provider.name}>
-                    <button className='w-full text-center py-3 rounded bg-brand-secondary text-white hover:bg-green-dark focus:outline-none my-1 text-xl' type="submit" onClick={() => signIn(provider.id)}> Sign in with: {provider.name}</button>
+                    <button className='w-full text-center py-3 px-24 rounded bg-brand-secondary text-white my-5 text-2xl' type="submit" onClick={() => signIn(provider.id)}> {provider.name} </button>
                     </div>
                   ))}
                   </div>
-              </form>
           </div>
           </div>
         
       </main>
-    //     <main className="flex">
-    //     <h1 className="font-primary text-brand-textHeader text-6xl">
-    //       {data.map(provider => (
-    //         <div key={provider.name}>
-    //             <button onClick={() => signIn(provider.id)}>
-    //                 Sign in with {provider.name}
-    //             </button>
-    //         </div>
-    //       ))}
-    //     </h1>
-    //   </main>
     )
 }
