0c012ffb2380886a8beabcc6b06fa83e8d5ecdda
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-07-13 16:59:31 -0400

    need to merge main

--- src/app/dashboard/components/SettingsPanel.js
+++ src/app/dashboard/components/SettingsPanel.js
@@ -1,30 +1,43 @@
 "use client";
-import { useQuery, gql } from "@apollo/client";
-import { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 const userQuery = gql`
-  query Query($user: ID!) {
-    getUserPortfolioByUser(user: $user) {
-      _id
-      user
-      firstName
-      lastName
-      title
-      bio
-      rate
-      portfolioUrl
-      githubUrl
-      linkedinUrl
-      calendlyUrl
-      available
-    }
+query User($id: ID!) {
+  user(ID: $id) {
+    _id
+    email
+    name
+    image
+    company
   }
+}
+`;
+
+const userMutation = gql`
+mutation Mutation($id: ID!, $name: String!, $email: String!, $image: String, $company: String) {
+  updateUser(ID: $id, name: $name, email: $email, image: $image, company: $company) {
+    _id
+    email
+    name
+    image
+    company
+  }
+}
 `;
 
 export default function ProfilePanel(props) {
   const [isEditable, setIsEditable] = useState(false);
+  const [userState, setUserState] = useState({});
+  const [updateUser] = useMutation(userMutation, { onCompleted(args) { onMutateComplete(args)}});
+
+  function onMutateComplete(args) {
+    setUserState({...args.updateUser});
+
+    setIsEditable(false);
+  }
 
   const handleEditClick = () => {
     setIsEditable(true);
@@ -36,9 +49,22 @@ export default function ProfilePanel(props) {
 
   const { data, error, loading } = useQuery(userQuery, {
     variables: {
-      user: props.user.id,
+      id: props.user.id,
     },
   });
+
+  useEffect(() => {
+    if (!loading) {
+      setUserState({...data.user})
+      console.log("loaded",userState);
+    }
+  }, [loading]);
+
+  const handleUserChange = (event) => {
+    let {name, value} = event.target;
+
+    setUserState({...userState, [name]: value});
+  }
 
   if (loading) {
     return (
@@ -94,7 +120,7 @@ export default function ProfilePanel(props) {
             <div className="flex">
               {/* First and Last Name Field */}
               <div className="flex flex-col w-1/2">
-                <label className="text-sm md:text-base">First Name</label>
+                <label className="text-sm md:text-base">Name</label>
                 <input
                   className={`font-primary text-brand-textHeader text-base md:text-lg my-1 mr-4 px-2 -mx-2 rounded-lg ${
                     isEditable
