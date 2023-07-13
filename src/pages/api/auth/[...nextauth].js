d640cbc4fe827dbbbbc055e277da7f0d0290a8ca
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-07-13 14:00:46 -0400

    added facebook provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import { mongoClient } from "../../../config/mongodb";
 
@@ -46,6 +47,10 @@ export const authOptions = {
                     role: GetUserRole(profile.email)
                 }
             }
+        }), 
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
         })
     ],
 
